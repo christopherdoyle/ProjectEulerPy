9ed5a70631df85851f2e51505025e667d7483433
Christopher Doyle <christophercormac@outlook.com>
2025-07-05 11:22:45 +0100

    Added tests for nth prime

--- tests/lib/test_prime.py
+++ tests/lib/test_prime.py
@@ -17,3 +17,16 @@ from problems.lib import prime
 def test_sieve_of_eratosthenes(n, expected):
     actual = prime.sieve_of_eratosthenes(n)
     assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (1, 2),
+        (2, 3),
+        (10001, 104743)
+    ]
+)
+def test_nth_prime(n, expected):
+    actual = prime.nth_prime(n)
+    assert expected == actual
