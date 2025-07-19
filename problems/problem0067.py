2ca3d963a091ad7bce5db1e98625aba8e5503530
Christopher Doyle <christophercormac@outlook.com>
2025-07-19 10:41:26 +0100

    0067 functional solution, also

--- problems/problem0067.py
+++ problems/problem0067.py
@@ -1,4 +1,5 @@
 import os
+from functools import reduce
 
 from problems.lib import cli, main_wrapper
 from problems import problem_data
@@ -41,14 +42,11 @@ def parse_line(line: str) -> list[int]:
     return result
 
 
-@main_wrapper
-def main():
-    problem_fpath = problem_data.get("0067_triangle.txt")
-
+def imp_solution(line_iter):
     # rely on triangle shape, and traverse bottom up, summing the max
     # as we go (O(N))
     triangle_last_row = None
-    for row in readlines_reverse(problem_fpath, line_fn=parse_line):
+    for row in line_iter:
         if not row:
             continue
 
@@ -68,6 +66,24 @@ def main():
     assert len(triangle_last_row) == 1, "Triange not triangle shaped"
     result = triangle_last_row[0]
     return result
+
+
+def func_solution(line_iter):
+    return (
+        reduce(
+            lambda xs, ys: [
+                y + max(xs[col_i], xs[col_i + 1]) for col_i, y in enumerate(ys)
+            ],
+            filter(lambda x: bool(x), line_iter),
+        )
+    )[0]
+
+
+@main_wrapper
+def main():
+    problem_fpath = problem_data.get("0067_triangle.txt")
+    line_iter = readlines_reverse(problem_fpath, line_fn=parse_line)
+    return func_solution(line_iter)
 
 
 if __name__ == "__main__":
